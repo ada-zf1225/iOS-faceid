31c562d34434cdc4cde6d07abe9ade46a15dd441
Fan Ziheng <zf1225@ic.ac.uk>
2026-06-07 20:18:26 +0100

    Initial Commit

---
+++ FaceID/FaceIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceIDApp.swift
+//  FaceID
+//
+//  Created by admin on 07/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct FaceIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FaceIDTests/FaceIDTests.swift
@@ -0,0 +1,17 @@
+//
+//  FaceIDTests.swift
+//  FaceIDTests
+//
+//  Created by admin on 07/06/2026.
+//
+
+import Testing
+@testable import FaceID
+
+struct FaceIDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
